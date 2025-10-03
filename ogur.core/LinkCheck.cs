ed1de81ccbe3b98af565d190f10b504051fd4441
ishkabar <dom.karczewski@gmail.com>
2025-10-03 19:28:35 +0200

    core: depend on ogur.Abstractions, add scheduler, package README in nupkg

--- ogur.core/LinkCheck.cs
+++ ogur.core/LinkCheck.cs
@@ -5,4 +5,4 @@ internal static class LinkCheck
 {
     public static (CapabilityStatus Status, BotEvent Event) Ping()
         => (CapabilityStatus.Running, new BotEvent(DateTimeOffset.UtcNow, "ping", "ok"));
-}asdasd
+}
